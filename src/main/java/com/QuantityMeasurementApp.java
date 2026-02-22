264cb8c92104812a17fba74c44623263aecc930c
vanshu0525 <vanshslathia03@gmail.com>
2026-02-22 19:09:39 +0530

    [Vansh] : added a quantity measurement app class to run the program

---
+++ src/main/java/com/QuantityMeasurementApp.java
@@ -0,0 +1,14 @@
+package com;
+
+public class QuantityMeasurementApp {
+	
+	public static void main(String[] args) {
+		
+		Feet f1 = new Feet(1.0);
+		Feet f2 = new Feet(1.0);
+		
+		System.out.println("Equals: " + f1.equals(f2));
+		
+	}
+
+}
