c4e4dfd0267d9bd4eaec1c01bd7cf2e0036cc13c
vanshu0525 <vanshslathia03@gmail.com>
2026-02-23 12:11:51 +0530

    [Vansh] : Update quantitymeasurementapp class to demonstrate length class check

--- src/main/java/com/QuantityMeasurementApp.java
+++ src/main/java/com/QuantityMeasurementApp.java
@@ -1,14 +1,32 @@
 package com;
 
 public class QuantityMeasurementApp {
-	
-	public static void main(String[] args) {
-		
-		Feet f1 = new Feet(1.0);
-		Feet f2 = new Feet(1.0);
-		
-		System.out.println("Equals: " + f1.equals(f2));
-		
+
+    public static boolean demonstrateLengthEquality(Length l1, Length l2) {
+        return l1.equals(l2);
+    }
+    
+    public static boolean demonstrateLengthComparison(double value1, Length.LengthUnit unit1, double value2,Length.LengthUnit unit2) {
+
+		Length l1 = new Length(value1, unit1);
+		Length l2 = new Length(value2, unit2);
+ 
+		boolean result = l1.equals(l2);
+
+		System.out.println("lengths are equal : " + result);
+		return result;
 	}
 
+    public static void main(String[] args) {
+
+		demonstrateLengthComparison(1.0, Length.LengthUnit.FEET, 12.0, Length.LengthUnit.INCHES);
+
+		demonstrateLengthComparison(1.0, Length.LengthUnit.YARDS, 3.0, Length.LengthUnit.FEET);
+
+		demonstrateLengthComparison(1.0, Length.LengthUnit.YARDS, 36.0, Length.LengthUnit.INCHES);
+
+		demonstrateLengthComparison(1.0, Length.LengthUnit.CENTIMETERS, 0.393701, Length.LengthUnit.INCHES);
+
+		demonstrateLengthComparison(2.0, Length.LengthUnit.YARDS, 6.0, Length.LengthUnit.FEET);
+    }
 }
